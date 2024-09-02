a43aa4f1ce68a65b4bc39183770becd739333d14
Faradaii <kdkfrdy@gmail.com>
2024-09-02 14:05:55 +0800

    feat: :sparkles: onboarding screen

---
+++ lib/data/onboarding.dart
@@ -0,0 +1,17 @@
+var onboardingData = [
+  {
+    "title": "Welcome to \n Savor!",
+    "image": "assets/images/onboarding_1.svg",
+    "desc": "Discover the best restaurants tailored to your taste."
+  },
+  {
+    "title": "Enjoy \n Your Favorites",
+    "image": "assets/images/onboarding_2.svg",
+    "desc": "Easily find your favorite food in the best restaurants." 
+  },
+  {
+    "title": "Let's Start to Discover",
+    "image": "assets/images/onboarding_3.svg",
+    "desc": "Dive in now and uncover your next favorite restaurant!"
+  },
+];
