8aef6ae7cb8306539c7c488df9b34694f042e3d2
Faradaii <kdkfrdy@gmail.com>
2024-09-13 12:35:22 +0800

    chore: :pushpin: update dependencies and configurations

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:savor/screens/bookmark_screen.dart';
 import 'package:savor/screens/detail_screen.dart';
 import 'package:savor/screens/home_screen.dart';
 import 'package:savor/screens/onboarding_screen.dart';
 import 'package:savor/screens/search_screen.dart';
+import 'package:savor/screens/settings_screen.dart';
 import 'package:savor/screens/splash_screen.dart';
 
 var routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
   OnboardingScreen.routeName: (context) => const OnboardingScreen(),
-  DetailScreen.routeName: (context) => DetailScreen(idRestaurant: ModalRoute.of(context)?.settings.arguments as String ),
+  DetailScreen.routeName: (context) => DetailScreen(
+      idRestaurant: ModalRoute.of(context)?.settings.arguments as String),
   SearchScreen.routeName: (context) => const SearchScreen(),
+  SettingsScreen.routeName: (context) => const SettingsScreen(),
+  BookmarkedScreen.routeName: (context) => const BookmarkedScreen(),
 };
 
 var initialRoute = SplashScreen.routeName;
